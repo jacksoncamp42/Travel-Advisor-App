92d8e08dbfafefeb5256b85a9039d35d98052c0d
Jackson Camp <jacksoncamp@Jacksons-MacBook-Pro.local>
2022-07-29 15:09:52 -0700

    Added pinpoints to map

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Box} from "@chakra-ui/react"
 import GoogleMapReact from "google-map-react"
+import { IoLocation } from 'react-icons/io5'
 
 const Map = ({setCoordinates, coordinates, setBounds}) => {
+  const[isCard, setIsCard] = useState(false)
+  const[cardData, setCardData] = useState(null)
+
   return (
   <Box width={"full"} height={"full"}>
     <GoogleMapReact
@@ -16,16 +20,37 @@ const Map = ({setCoordinates, coordinates, setBounds}) => {
         setCoordinates({lat: e.center.lat, lng: e.center.lng})
         setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
       }}
-      onChildClick={() => {}}
+      onChildClick={(child) => {
+        setCardData(places[child])
+        setIsCard(true)
+      }}
     >
       {places?.map((place, i) => {
-        <Box>
+        <Box
           lat={Number(place.latitude)}
           lng={Number(place.longitude)}
+          position={'relative'}
+          cursor='pointer'
+        >
+          <IoLocation color="red" fontSize={30}/>
         </Box>
       })}
+      {isCard && (
+        <Box 
+        width={'200px'}
+        height={'150px'}
+        bg={'whiteAlpha.900'}
+        position={'absolute'}
+        top={-12}
+        left={0}
+        shadow={'lg'}
+        rounded={'lg'}
+        >
+        
+        </Box>
+      )}
     </GoogleMapReact>
-</Box>)
+  </Box>)
 }
 
 export default Map
